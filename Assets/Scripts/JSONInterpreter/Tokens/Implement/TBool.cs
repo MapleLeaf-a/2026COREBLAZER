a51a7486c7a8cbd9bdd6130a996b5b7c03816f81
SkySakura2005 <wuxinyang89@gmail.com>
2026-03-11 02:44:31 +0800

    初步写了一些JSON词法分析器，编译原理搞得相似

---
+++ Assets/Scripts/JSONInterpreter/Tokens/Implement/TBool.cs
@@ -0,0 +1,13 @@
+﻿using JSONInterpreter.Tokens.Interface;
+
+namespace JSONInterpreter.Tokens.Implement
+{
+    public class TBool:IToken
+    {
+        public TBool(bool b)
+        {
+            value=b;
+        }
+        public bool value;
+    }
+}
---
+++ Assets/Scripts/JSONInterpreter/Tokens/Implement/TString.cs
@@ -0,0 +1,13 @@
+﻿using JSONInterpreter.Tokens.Interface;
+
+namespace JSONInterpreter.Tokens.Implement
+{
+    public class TString: IToken
+    {
+        public TString(string s)
+        {
+            value=s;
+        }
+        public string value;
+    }
+}
