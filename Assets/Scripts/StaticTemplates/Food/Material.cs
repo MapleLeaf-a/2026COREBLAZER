5730b48002110a791105cccb704845c332f89d9f
SkySakura2005 <wuxinyang89@gmail.com>
2026-03-31 20:44:41 +0800

    Create Material.cs

---
+++ Assets/Scripts/StaticTemplates/Food/Material.cs
@@ -0,0 +1,9 @@
+namespace StaticTemplates.MusicGame
+{
+    public class Food:IBaseJsonInstance
+    {
+        public string name;
+        public string spritePath;
+        public string description;
+    }
+}
