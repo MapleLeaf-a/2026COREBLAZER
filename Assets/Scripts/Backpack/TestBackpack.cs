cc8c6efc1add4fe62800f6686f1e7c5642c98903
MapleLeaf-a <3453119782@qq.com>
2026-04-11 23:13:02 +0800

    修复了测试脚本的bug

--- Assets/Scripts/Backpack/TestBackpack.cs
+++ Assets/Scripts/Backpack/TestBackpack.cs
@@ -26,19 +26,21 @@ public class TestBackpack : MonoBehaviour
         }
 
         FoodMaterial material = new FoodMaterial("114", "KaZuHa", "Images/kazuha", "枫原万叶,流浪的武士", StaticTemplates.Food.MaterialType.Drink);
-        BagItem bagItem = new BagItem(material, 1);
         FoodMaterial material2 = new FoodMaterial("514", "GGG", "Images/GoTo", "Go Go Go,出发咯", StaticTemplates.Food.MaterialType.Drink);
+        BagItem bagItem = new BagItem(material, 1);
         BagItem bagItem2 = new BagItem(material2, 2);
-        FoodMaterial material3 = new FoodMaterial("114", "KaZuHa", "Images/kazuha", "枫原万叶,流浪的武士", StaticTemplates.Food.MaterialType.Drink);
-        BagItem bagItem3 = new BagItem(material3, 1);
+        BagItem bagItem3 = new BagItem(material, 1);
         backpackModel = new BackpackModel(20);
         backpackModel.AddItem(bagItem);
         backpackModel.AddItem(bagItem2);
         backpackModel.AddItemAt(bagItem3, 3);
+        BagItem bagItem4 = new BagItem(material, 2);
+        BagItem bagItem5 = new BagItem(material2, 3);
+        BagItem bagItem6 = new BagItem(material, 2);
         backpackMode2 = new BackpackModel(20);
-        backpackMode2.AddItem(bagItem);
-        backpackMode2.AddItem(bagItem2);
-        backpackMode2.AddItemAt(bagItem3, 3);
+        backpackMode2.AddItem(bagItem4);
+        backpackMode2.AddItem(bagItem5);
+        backpackMode2.AddItemAt(bagItem6, 3);
 
         backpackView1.InitBackpackView(backpackModel);
         backpackView2.InitBackpackView(backpackMode2);
