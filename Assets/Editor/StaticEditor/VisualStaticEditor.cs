8de38be6a2c92edd057f175cf30622da90a7f647
SkySakura2005 <wuxinyang89@gmail.com>
2026-04-12 16:47:27 +0800

    增加读取类的逻辑

--- Assets/Editor/StaticEditor/VisualStaticEditor.cs
+++ Assets/Editor/StaticEditor/VisualStaticEditor.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -19,12 +23,33 @@ public class VisualStaticEditor : EditorWindow
         // Each editor window contains a root VisualElement object
         VisualElement root = rootVisualElement;
 
-        // VisualElements objects can contain other VisualElement following a tree hierarchy.
-        //VisualElement label = new Label("Hello World! From C#");
-        //root.Add(label);
+        TwoPaneSplitView splitView = new TwoPaneSplitView(0,250,TwoPaneSplitViewOrientation.Horizontal);
+        
+        root.Add(splitView);
+        
+        ListView leftTreeList = new ListView();
+        VisualElement rightPanel = new VisualElement();
 
-        // Instantiate UXML
-        VisualElement labelFromUXML = m_VisualTreeAsset.Instantiate();
-        root.Add(labelFromUXML);
+        List<Type> types = GetClassList();
+        
+        leftTreeList.makeItem = ()=> new Label();
+        leftTreeList.bindItem = (item, index) =>
+        {
+            if (item is Label label) label.text = types[index].Name;
+        };
+        leftTreeList.itemsSource = types;
+        
+        splitView.Add(leftTreeList);
+        splitView.Add(rightPanel);
+        
+    }
+
+    private List<Type> GetClassList()
+    {
+        Assembly assembly = Assembly.GetExecutingAssembly();
+        
+        var classTypes = assembly.GetTypes()
+            .Where(t => t.IsClass && t.Namespace != null && ( t.Namespace == "StaticTemplates"||t.Namespace.StartsWith("StaticTemplates.")));
+        return classTypes.ToList();
     }
 }
