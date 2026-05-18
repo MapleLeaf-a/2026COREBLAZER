95b4ddca354d214ac52edc75124984aea80e2a21
扭矩不足 <1214113371@qq.com>
2026-05-18 11:45:30 +0800

    EventSystem,场景切换

---
+++ Assets/Scripts/Tools/SlideInjector/ProjectFlowInjector.cs
@@ -0,0 +1,16 @@
+﻿using Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Tools.SlideInjector
+{
+	public static class ProjectFlowInjector
+	{
+		public static void Init()
+		{
+		}
+	}
+}
